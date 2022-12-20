c9570e172bec7311dd57a67c96a35623ded4f658
Yoo Keoung Oh <dhdbrud318@gmail.com>
2022-12-19 18:34:52 -0800

    Added Icon Effects

--- src/components/common/check.js
+++ src/components/common/check.js
@@ -3,15 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle as emptyCircle } from "@fortawesome/free-regular-svg-icons";
 import { faCircle as fullCircle } from "@fortawesome/free-solid-svg-icons";
 
-//import circle from "../../assets/images/icon-check.svg";
+import { ReactComponent as CheckIcon } from "../../assets/images/icon-check.svg";
 
 function Check({ id, status, onClick }) {
   return (
     <button onClick={onClick} value={status} id={id} className="check-btn">
-      <FontAwesomeIcon
-        className="checkIcon"
-        icon={status === "active" ? emptyCircle : fullCircle}
-      />
+      <div
+        className={
+          status === "completed"
+            ? "check-border flex active"
+            : "check-border flex"
+        }
+      >
+        <CheckIcon className="check-icon" />
+      </div>
     </button>
   );
 }
